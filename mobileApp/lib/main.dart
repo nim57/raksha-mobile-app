eb73135aaff026d0b4acf815c3657741bea98234
Nimesh Sandaruwan <119092886+nim57@users.noreply.github.com>
2026-06-28 20:18:49 +0530

    firebase initiation

--- mobileApp/lib/main.dart
+++ mobileApp/lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'features/onboarding/presentation/screens/onboarding_screen1.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
